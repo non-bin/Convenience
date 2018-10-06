bfd9f1720ce540002de41a50ff6182973a363305
Alice Jacka <harry@jacka.net.au>
2018-10-06 13:15:05 +1000

    tmpfile will now only execute if index.js exits with 0

--- convenience/convenience.sh
+++ convenience/convenience.sh
@@ -1,2 +1,9 @@
-node ./index.js
-/tmp/convenience.sh
+# run the js protion to select an action,
+# then if it exits 0, execute the requested
+# command from the temp file
+
+if node ~/repos/Convenience/convenience/index.js
+then
+    chmod +x /tmp/convenience.sh
+    /tmp/convenience.sh
+fi
