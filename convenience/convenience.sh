cd960e7255e7b0503f6520941d8e22669082379a
Alice Jacka <harry@jacka.net.au>
2018-10-11 20:18:34 +1100

    args are passed to js portion from sh script (#6)

--- convenience/convenience.sh
+++ convenience/convenience.sh
@@ -2,7 +2,7 @@
 # then if it exits 0, execute the requested
 # command from the temp file
 
-if node ~/repos/Convenience/convenience/index.js
+if node ~/repos/Convenience/convenience/index.js "$@"
 then
     chmod +x /tmp/convenience.sh
     . /tmp/convenience.sh
